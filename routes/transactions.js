715b9380497afd444a538541f9cfdcd38287f9bf
TAKANOME-DEV <79809121+TAKANOME-DEV@users.noreply.github.com>
2021-10-06 17:34:33 +0000

    config cors

--- routes/transactions.js
+++ routes/transactions.js
@@ -7,11 +7,24 @@ const {
   deleteTransaction,
 } = require("../controllers/transactions");
 
+const allowlist = ["https://expense-tracker-frontend.netlify.app/"];
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 router
   .route("/transactions")
-  .get(cors(), getTransactions)
-  .post(cors(), addTransaction);
+  .get(cors(corsOptionsDelegate), getTransactions)
+  .post(cors(corsOptionsDelegate), addTransaction);
 
-router.route("/transactions/:id").delete(cors(), deleteTransaction);
+router
+  .route("/transactions/:id")
+  .delete(cors(corsOptionsDelegate), deleteTransaction);
 
 module.exports = router;
